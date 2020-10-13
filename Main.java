7667234a98ab66e7907a2146c2ab1cf76e1731cb
Croth124 <cr321614@g.risd.org>
2020-10-13 23:22:38 +0000

    Not Much

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main { public static void main(String[] args) {
+
+  Scanner usIn = new Scanner(System.in);
+
+
+  String usString = new usIn.nextLine();
+  System.out.print(usString);
+
+ } }
